fc3a88dbc7bf1a5982cbd82993a44dde123a5484
JonyTask <yuusu.hyb@gmail.com>
2023-12-21 22:24:13 +0900

    favoritesテーブルにお気に入り追加機能

---
+++ src/app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Favorite;
+
+class FavoriteController extends Controller
+{
+    public function ControlFavorite(Request $request){
+        $favorite_field = Favorite::where('user_id',Auth::id())->where('gourmet_id',$request->gourmet_id)->first();
+        if(!$favorite_field){
+            Favorite::create([
+                'user_id' => Auth::id(),
+                'gourmet_id' => $request->gourmet_id,
+            ]);
+        }else{
+            Favorite::destroy($favorite_field->id);
+        }
+        return redirect('/');
+    }
+}
--- src/resources/views/gourmets.blade.php
+++ src/resources/views/gourmets.blade.php
@@ -13,9 +13,18 @@
                     <p class="gourmet_name">{{ $gourmet->gourmet_name }}</p>
                     <span class="area_tag">#{{ $gourmet->areas->gourmet_area }}</span>
                     <span class="genre_tag">#{{ $gourmet->genres->gourmet_genre }}</span>
-                    <form class="view_detail" method="post" action="">
-                        <button class="detail_button" type="submit">詳しく見る</button>
-                    </form>
+                    <div class="flex-area">
+                        <form class="view_detail" method="post" action="/gourmet_detail">
+                            @csrf
+                            <input type="hidden" name="gourmet_id" value="{{ $gourmet->id }}" readonly>
+                            <button class="detail_button" type="submit">詳しく見る</button>
+                        </form>
+                        <form action="/favorite_gourmet" method="post">
+                            @csrf
+                            <input type="hidden" name="gourmet_id" value="{{ $gourmet->id }}" readonly>
+                            <button type="submit" class="heart"></button>
+                        </form>
+                    </div>
                 </div>
             </div>
         @endforeach
