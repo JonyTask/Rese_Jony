7cb5579ee828dc13282e8d6ed19061f88e930f81
JonyTask <yuusu.hyb@gmail.com>
2023-12-21 22:26:43 +0900

    認証画面

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.share')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+@endsection
+
+@section('main')
+    <div class="login_form_area">
+        <div class="login_upper">
+            <p class="form_title">Login</p>
+        </div>
+        <div class="login_under">
+            <form action="/login" class="login_form" method="post">
+                @csrf
+                <ul class="login_form_items">
+                    <li class="login_form_item">
+                        <input type="email" class="Email_Input" placeholder="Email" name="email">
+                    </li>
+                    <li class="login_form_item">
+                        <input type="password" class="Password_Input" placeholder="Password" name="password">
+                    </li>
+                </ul>
+                <div class="login_submit_area">
+                    <button type="submit" class="login_submit_button">ログイン</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.share')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('assets/css/register.css') }}">
+@endsection
+
+@section('main')
+    <div class="register_form_area">
+        <div class="register_upper">
+            <p class="form_title">Registration</p>
+        </div>
+        <div class="register_under">
+            <form action="/register" class="register_form" method="post">
+                @csrf
+                <ul class="register_form_items">
+                    <li class="register_form_item">
+                        <input type="text" class="Name_Input" placeholder="Name" name="name">
+                    </li>
+                    <li class="register_form_item">
+                        <input type="email" class="Email_Input" placeholder="Email" name="email">
+                    </li>
+                    <li class="login_form_item">
+                        <input type="password" class="Password_Input" placeholder="Password" name="password">
+                    </li>
+                    <li class="register_form_item">
+                        <input type="password" class="Confirmation_Input" placeholder="Password Confirmation" name="password_confirmation">
+                    </li>
+                </ul>
+                <div class="register_submit_area">
+                    <button type="submit" class="register_submit_button">登録</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
