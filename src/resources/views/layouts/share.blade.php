c082cfdcf97e5817d08386d0c2ff8e9e01255b8f
JonyTask <yuusu.hyb@gmail.com>
2023-12-30 20:08:19 +0900

    親レイアウトの作成

---
+++ src/resources/views/layouts/share.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rese</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/common.css') }}">
+    @yield('css')
+</head>
+<body class="body_zone">
+    <div class="header">
+        <div class="header_inner">
+            <div class="humberger_flex">
+                <div class="hamburger_menu">
+                    <span class="hamburger_line line_SecondOne"></span>
+                    <span class="hamburger_line line_FirstOne"></span>
+                    <span class="hamburger_line line_ForthOne"></span>
+                </div>
+                <h1 class="site-title">Rese</h1>
+            </div>
+            @yield('search')
+        </div>
+    </div>
+    @yield('main')
+</body>
+</html>
