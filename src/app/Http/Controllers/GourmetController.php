712701934b2c75d11b0038f4f38cb37af6039141
JonyTask <yuusu.hyb@gmail.com>
2024-01-01 16:32:05 +0900

    効率的なSQL記述

--- src/app/Http/Controllers/GourmetController.php
+++ src/app/Http/Controllers/GourmetController.php
@@ -8,6 +8,7 @@ use App\Models\Gourmet;
 use App\Models\Area;
 use App\Models\Genre;
 use App\Models\Favorite;
+use App\Models\Reserve;
 use App\MyFuncs\MyFuncs;
 
 class GourmetController extends Controller
@@ -25,5 +26,17 @@ class GourmetController extends Controller
     public function ViewDetail(Request $request){
         $gourmet = Gourmet::with(['areas','genres'])->find($request->gourmet_id);
         return view('detail',compact('gourmet'));
+    }
+
+    public function ViewMyPage(){
+        $reserves = Reserve::where('user_id',Auth::id())->with('gourmets:id,name')->get();
+
+        $gourmets = Gourmet::where(function($query){
+            $favorites = Favorite::where('user_id',Auth::id())->get('gourmet_id');
+            $favorites_array = MyFuncs::CreateArray($favorites);
+            return $query->whereIn('gourmets.id',$favorites_array);
+        })->with(['areas','genres'])->get();
+
+        return view('mypage',compact('reserves','gourmets'));
     }
 }
